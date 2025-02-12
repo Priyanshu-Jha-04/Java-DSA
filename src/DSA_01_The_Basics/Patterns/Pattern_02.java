3b03b25e4fff37ea6aba758e290cfa20a16272cb
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-13 00:44:50 +0530

    Update Pattern_02.java

--- src/DSA_01_The_Basics/Patterns/Pattern_02.java
+++ src/DSA_01_The_Basics/Patterns/Pattern_02.java
@@ -7,20 +7,14 @@ public class Pattern_02 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number of test cases : ");
-        int t = sc.nextInt(); // No. of test cases
+        System.out.println("Enter the number of rows : ");
+        int n = sc.nextInt();
 
-        for (int k = 0; k < t; k++) {
-
-            System.out.println("Enter the number of rows : ");
-            int n = sc.nextInt();
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j <= i; j++) {
-                    System.out.print("* ");
-                }
-                System.out.println();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
             }
+            System.out.println();
         }
     }
 }
