e039b9b4acfa633b9df251796ba14b97645e8398
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-13 00:14:13 +0530

    Update Pattern_01.java

--- src/DSA_01_The_Basics/Patterns/Pattern_01.java
+++ src/DSA_01_The_Basics/Patterns/Pattern_01.java
@@ -1,4 +1,20 @@
 package DSA_01_The_Basics.Patterns;
 
+import java.util.Scanner;
+
 public class Pattern_01 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the pattern : ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
