10990ce20037f6f63e5f102d79892da325dec74c
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-13 00:45:36 +0530

    Update Pattern_03.java

--- src/DSA_01_The_Basics/Patterns/Pattern_03.java
+++ src/DSA_01_The_Basics/Patterns/Pattern_03.java
@@ -1,7 +1,21 @@
 package DSA_01_The_Basics.Patterns;
 
+import java.util.Scanner;
+
 public class Pattern_03 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the number of rows : ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int val = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(val);
+                val++;
+            }
+            System.out.println();
+        }
     }
 }
