321e8138044a0d6b2e84b382ccc926bd03993c28
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-13 23:00:59 +0530

    Create Pattern_08.java

---
+++ src/DSA_01_The_Basics/Patterns/Pattern_08.java
@@ -0,0 +1,27 @@
+package DSA_01_The_Basics.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_08 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows : ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2*n -(2*i+1)); j++) {
+                System.out.print("*");
+
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
