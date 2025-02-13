9cb4c175f3ae41e93cd7df365faa62d027b446ad
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-13 22:53:52 +0530

    Update Pattern_07.java

--- src/DSA_01_The_Basics/Patterns/Pattern_07.java
+++ src/DSA_01_The_Basics/Patterns/Pattern_07.java
@@ -11,13 +11,13 @@ public class Pattern_07 {
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < (n - i -1); j++) {
+            for (int j = 0; j < (n - i - 1); j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < (2*i+1); j++) {
+            for (int j = 0; j < (2 * i + 1); j++) {
                 System.out.print("*");
             }
-            for (int j = 0; j < (n - i -1); j++) {
+            for (int j = 0; j < (n - i - 1); j++) {
                 System.out.print(" ");
             }
             System.out.println();
