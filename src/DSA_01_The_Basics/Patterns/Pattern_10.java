ddadba485e7b3dcb993283ce27f9ca80a9dac528
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-14 00:46:48 +0530

    Update Pattern_10.java

--- src/DSA_01_The_Basics/Patterns/Pattern_10.java
+++ src/DSA_01_The_Basics/Patterns/Pattern_10.java
@@ -8,15 +8,15 @@ public class Pattern_10 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the peak row : ");
-        int N = sc.nextInt();
+        int n = sc.nextInt();
 
-        for(int i=1;i<=2*N-1;i++){
+        for (int i = 1; i <= 2 * n - 1; i++) {
 
             int stars = i;
 
-            if(i>N) stars = 2*N-i;
+            if (i > n) stars = 2 * n - i;
 
-            for(int j=1;j<=stars;j++){
+            for (int j = 1; j <= stars; j++) {
                 System.out.print("*");
             }
             System.out.println();
