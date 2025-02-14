ffe18c7dd5a8e0b38ac7edfda4233aab14aec3ff
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-14 23:40:59 +0530

    Create Pattern_20.java

---
+++ src/DSA_01_The_Basics/Patterns/Pattern_20.java
@@ -0,0 +1,26 @@
+package DSA_01_The_Basics.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_20 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows : ");
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = 1; j <= ((2 * n) - (2 * i)); j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
