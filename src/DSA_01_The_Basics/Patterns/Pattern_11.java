6826d7e5990f53b9a8e564b6badf8b3d2389627e
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-14 21:53:09 +0530

    Create Pattern_11.java

---
+++ src/DSA_01_The_Basics/Patterns/Pattern_11.java
@@ -0,0 +1,29 @@
+package DSA_01_The_Basics.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_11 {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the peak row : ");
+        int n = sc.nextInt();
+
+        int start = 1;
+
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 0) {
+                start = 0;
+            } else {
+                start = 1;
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(start + " ");
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+}
