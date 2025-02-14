db640a6c3f4cadcd2eef5823c5da87c03a4f1bf4
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-14 23:19:15 +0530

    Update Pattern_18.java

--- src/DSA_01_The_Basics/Patterns/Pattern_18.java
+++ src/DSA_01_The_Basics/Patterns/Pattern_18.java
@@ -1,7 +1,23 @@
 package DSA_01_The_Basics.Patterns;
 
+import java.util.Scanner;
+
 public class Pattern_18 {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows : ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            char ch = 'A';
+            ch = (char) (ch + (n - i - 1));
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
     }
 }
