c628c1d85cf844ba4fb2e6cc12297e325c765c72
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-14 23:54:34 +0530

    Update Pattern_20.java

--- src/DSA_01_The_Basics/Patterns/Pattern_20.java
+++ src/DSA_01_The_Basics/Patterns/Pattern_20.java
@@ -9,23 +9,23 @@ public class Pattern_20 {
 
         System.out.println("Enter the number of rows : ");
         int n = sc.nextInt();
-        int spaces = 2*n-2;
+        int spaces = 2 * n - 2;
 
-        for(int i = 1;i<=2*n-1;i++){
+        for (int i = 1; i <= 2 * n - 1; i++) {
             int stars = i;
-            if(i>n) stars = 2*n - i;
-            for(int j=1;j<=stars;j++){
+            if (i > n) stars = 2 * n - i;
+            for (int j = 1; j <= stars; j++) {
                 System.out.print("* ");
             }
-            for(int j = 1;j<=spaces;j++){
+            for (int j = 1; j <= spaces; j++) {
                 System.out.print(" ");
             }
-            for(int j = 1;j<=stars;j++){
+            for (int j = 1; j <= stars; j++) {
                 System.out.print("* ");
             }
             System.out.println();
-            if(i<n) spaces -=2;
-            else spaces +=2;
+            if (i < n) spaces -= 2;
+            else spaces += 2;
         }
     }
 }
