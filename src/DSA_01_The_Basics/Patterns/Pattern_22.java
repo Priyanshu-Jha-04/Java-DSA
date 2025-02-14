e8d61f4475a9c4ef8574d405b160bba300ff7325
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-15 00:20:20 +0530

    Update Pattern_22.java

--- src/DSA_01_The_Basics/Patterns/Pattern_22.java
+++ src/DSA_01_The_Basics/Patterns/Pattern_22.java
@@ -1,7 +1,26 @@
 package DSA_01_The_Basics.Patterns;
 
+import java.util.Scanner;
+
+import static java.lang.Math.min;
+
 public class Pattern_22 {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows : ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < 2 * n - 1; i++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                int top = i;
+                int bottom = j;
+                int right = (2 * n - 2) - j;
+                int left = (2 * n - 2) - i;
+                System.out.print(n - min(min(top, bottom), min(left, right)) + " ");
+            }
+            System.out.println();
+        }
     }
 }
