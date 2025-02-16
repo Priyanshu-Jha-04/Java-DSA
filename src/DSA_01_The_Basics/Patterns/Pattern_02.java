74f558e927b8162a29718c54ecf2ed802bf4c0a9
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-16 21:24:48 +0530

    Update Pattern_02.java

--- src/DSA_01_The_Basics/Patterns/Pattern_02.java
+++ src/DSA_01_The_Basics/Patterns/Pattern_02.java
@@ -11,6 +11,7 @@ public class Pattern_02 {
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
+            
             for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
