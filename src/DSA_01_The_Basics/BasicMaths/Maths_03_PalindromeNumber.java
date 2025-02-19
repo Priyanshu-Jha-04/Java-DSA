a8aeb7ab6d1bf746394c733c3bf3ce23a8dd098e
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-19 22:18:52 +0530

    Update Maths_03_PalindromeNumber.java

--- src/DSA_01_The_Basics/BasicMaths/Maths_03_PalindromeNumber.java
+++ src/DSA_01_The_Basics/BasicMaths/Maths_03_PalindromeNumber.java
@@ -4,24 +4,23 @@ import java.util.Scanner;
 
 public class Maths_03_PalindromeNumber {
 
-    public static int ReverseTheNumber(int n) {
-        int digits = 0;
+    public static boolean isPalindrome(int n) {
         int reverseNumber = 0;
-        while (n > 0) {
-            int lastDigit = n % 10;
-            digits++;
-            n = n / 10;
+        int num = n;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            num = num / 10;
             reverseNumber = (reverseNumber * 10) + lastDigit;
         }
-        return reverseNumber;
+        return n == reverseNumber;
     }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int k = sc.nextInt();
 
-        if (ReverseTheNumber(k) == k) {
+        if (isPalindrome(k)) {
             System.out.println("The number " + k + " is a Palindrome number.");
         } else {
             System.out.println("The number " + k + " is not a Palindrome number.");
