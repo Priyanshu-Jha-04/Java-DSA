0320c20e558bc17901221100b66f9cb13ae8a3bd
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-19 21:42:45 +0530

    Update Maths_01_CountDigits.java

--- src/DSA_01_The_Basics/BasicMaths/Maths_01_CountDigits.java
+++ src/DSA_01_The_Basics/BasicMaths/Maths_01_CountDigits.java
@@ -1,7 +1,25 @@
 package DSA_01_The_Basics.BasicMaths;
 
-public class Maths_01_CountDigits {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Maths_01_CountDigits {
+
+    public static int CountDigits(int number) {
+        int digits = 0;
+        while (number > 0) {
+            digits++;
+            number = number/10;
+        }
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int k = sc.nextInt();
+
+        int n = CountDigits(k);
+
+        System.out.println("The number " + k + " has " + n + " digits.");
     }
 }
