2e32904e75b2e3b10f9c6622e1c9f1764b88511e
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-19 21:49:59 +0530

    Update Maths_02_ReverseNumber.java

--- src/DSA_01_The_Basics/BasicMaths/Maths_02_ReverseNumber.java
+++ src/DSA_01_The_Basics/BasicMaths/Maths_02_ReverseNumber.java
@@ -1,7 +1,28 @@
 package DSA_01_The_Basics.BasicMaths;
 
+
+import java.util.Scanner;
+
 public class Maths_02_ReverseNumber {
+    public static int ReverseTheNumber(int n) {
+        int digits = 0;
+        int reverseNumber = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            digits++;
+            n = n / 10;
+            reverseNumber = (reverseNumber * 10) + lastDigit;
+        }
+        return reverseNumber;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int k = sc.nextInt();
+
+        int n = ReverseTheNumber(k);
+
+        System.out.println("The reversed number is " + n);
     }
 }
