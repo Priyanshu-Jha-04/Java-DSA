0133f34f05fdf28998026dec58d90eb720483d65
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-23 22:07:41 +0530

    Update Maths_04_GCD.java

--- src/DSA_01_The_Basics/BasicMaths/Maths_04_GCD.java
+++ src/DSA_01_The_Basics/BasicMaths/Maths_04_GCD.java
@@ -1,6 +1,11 @@
 package DSA_01_The_Basics.BasicMaths;
 
+import java.util.Scanner;
+
 public class Maths_04_GCD {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int k = sc.nextInt();
     }
 }
