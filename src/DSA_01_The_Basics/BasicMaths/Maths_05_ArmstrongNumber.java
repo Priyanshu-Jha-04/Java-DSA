f0bf83c879c6ea0feda29e9d7801118a251154c7
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-23 22:07:52 +0530

    Update Maths_05_ArmstrongNumber.java

--- src/DSA_01_The_Basics/BasicMaths/Maths_05_ArmstrongNumber.java
+++ src/DSA_01_The_Basics/BasicMaths/Maths_05_ArmstrongNumber.java
@@ -1,6 +1,11 @@
 package DSA_01_The_Basics.BasicMaths;
 
+import java.util.Scanner;
+
 public class Maths_05_ArmstrongNumber {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int k = sc.nextInt();
     }
 }
