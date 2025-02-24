c07969303602b94dfbe26fb8ed9fe8a6bae2211b
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-25 01:22:39 +0530

    Create Recursion_05_SumOfNaturalNumbers.java

---
+++ src/DSA_01_The_Basics/BasicRecursion/Recursion_05_SumOfNaturalNumbers.java
@@ -0,0 +1,22 @@
+package DSA_01_The_Basics.BasicRecursion;
+
+import java.util.Scanner;
+
+public class Recursion_05_SumOfNaturalNumbers {
+
+    public static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+
+        System.out.println("Sum of first " + n + " numbers: " + sum(n));
+    }
+}
