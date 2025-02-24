d3d4b2309fd4e2d482715d78b6371ec98b7e6fb2
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-25 00:44:21 +0530

    Create Recursion_03_NaturalNumber.java

---
+++ src/DSA_01_The_Basics/BasicRecursion/Recursion_03_NaturalNumber.java
@@ -0,0 +1,21 @@
+package DSA_01_The_Basics.BasicRecursion;
+
+import java.util.Scanner;
+
+public class Recursion_03_NaturalNumber {
+
+    static void naturalNumbers(int a, int b) {
+        if (a>b) return;
+        System.out.println(a);
+        naturalNumbers(a+1,b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+
+        naturalNumbers(1,n);
+    }
+}
