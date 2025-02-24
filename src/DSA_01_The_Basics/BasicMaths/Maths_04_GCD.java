cf4a7fe3e7b4c33400feb4058bc17982c760d829
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-24 22:46:03 +0530

    Update Maths_04_GCD.java

--- src/DSA_01_The_Basics/BasicMaths/Maths_04_GCD.java
+++ src/DSA_01_The_Basics/BasicMaths/Maths_04_GCD.java
@@ -3,9 +3,28 @@ package DSA_01_The_Basics.BasicMaths;
 import java.util.Scanner;
 
 public class Maths_04_GCD {
+
+    public static int gcd(int a, int b) {
+        int max = Math.max(a, b);
+        int gcd = 1;
+        for (int i = 1; i <= max; i++) {
+            if (a % i == 0 & b % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number: ");
+
+        System.out.print("Enter the first number: ");
         int k = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int l = sc.nextInt();
+
+        System.out.println("GCD = " + gcd(k, l));
+
     }
 }
