e124254fde45e7c6ac95a88a1a287a5ea6a2e5ea
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-24 22:19:57 +0530

    Update Maths_07_PrimeNumber.java

--- src/DSA_01_The_Basics/BasicMaths/Maths_07_PrimeNumber.java
+++ src/DSA_01_The_Basics/BasicMaths/Maths_07_PrimeNumber.java
@@ -2,7 +2,7 @@ package DSA_01_The_Basics.BasicMaths;
 
 import java.util.Scanner;
 
-public class Maths_07_PrimeNumbers {
+public class Maths_07_PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
