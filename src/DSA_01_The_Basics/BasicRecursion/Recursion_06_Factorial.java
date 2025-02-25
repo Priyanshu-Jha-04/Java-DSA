3d4f542fba7a68f0fc6479fa3d439718d72bbd80
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-25 22:07:08 +0530

    Update Recursion_06_Factorial.java

--- src/DSA_01_The_Basics/BasicRecursion/Recursion_06_Factorial.java
+++ src/DSA_01_The_Basics/BasicRecursion/Recursion_06_Factorial.java
@@ -7,8 +7,8 @@ public class Recursion_06_Factorial {
     static int factorial;
 
     public static int factorial(int n) {
-        if (n == 0 || n ==1) return 1;
-        else return n*factorial(n-1);
+        if (n == 0 || n == 1) return 1;
+        else return n * factorial(n - 1);
     }
 
     public static void main(String[] args) {
