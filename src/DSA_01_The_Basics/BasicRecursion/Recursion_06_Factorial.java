78575d0a0e251454ee76ab7c44b8aea3ed00882c
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-25 22:06:58 +0530

    Create Recursion_06_Factorial.java

---
+++ src/DSA_01_The_Basics/BasicRecursion/Recursion_06_Factorial.java
@@ -0,0 +1,22 @@
+package DSA_01_The_Basics.BasicRecursion;
+
+import java.util.Scanner;
+
+public class Recursion_06_Factorial {
+
+    static int factorial;
+
+    public static int factorial(int n) {
+        if (n == 0 || n ==1) return 1;
+        else return n*factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+
+        System.out.println("Factorial of " + n + " is " + factorial(n));
+    }
+}
