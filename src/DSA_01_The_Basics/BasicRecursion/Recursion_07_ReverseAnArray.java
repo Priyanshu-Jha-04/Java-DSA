bf69488f02ed3f7eb1a886e0b53ab789b5e2f404
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-25 23:00:33 +0530

    Update Recursion_07_ReverseAnArray.java

--- src/DSA_01_The_Basics/BasicRecursion/Recursion_07_ReverseAnArray.java
+++ src/DSA_01_The_Basics/BasicRecursion/Recursion_07_ReverseAnArray.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 
 public class Recursion_07_ReverseAnArray {
 
-    public static void reverseArray(int start, int end,int[] arr) {
-        if (start>=end) return;
+    public static void reverseArray(int start, int end, int[] arr) {
+        if (start >= end) return;
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
-        reverseArray(start+1,end-1,arr);
+        reverseArray(start + 1, end - 1, arr);
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1, 2, 3, 4, 5};
         System.out.print("Original Array: ");
         System.out.println(Arrays.toString(arr));
 
-        reverseArray(0,4,arr);
+        reverseArray(0, 4, arr);
         System.out.print("Reversed Array: ");
         System.out.println(Arrays.toString(arr));
     }
