b44e2a661f522b23d78c02f27c6f52b96e8661be
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-27 21:54:48 +0530

    Update Hashing_01_CountingFreqencies.java

--- src/DSA_01_The_Basics/BasicHashing/Hashing_01_CountingFreqencies.java
+++ src/DSA_01_The_Basics/BasicHashing/Hashing_01_CountingFreqencies.java
@@ -23,29 +23,24 @@ public class Hashing_01_CountingFreqencies {
         }
     }
 
-    public static void Frequency(int arr[], int n)
-    {
+    public static void Frequency(int arr[], int n) {
         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < n; i++)
-        {
-            if (map.containsKey(arr[i]))
-            {
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
                 map.put(arr[i], map.get(arr[i]) + 1);
-            }
-            else
-            {
+            } else {
                 map.put(arr[i], 1);
             }
         }
-        for (Map.Entry<Integer,Integer> entry : map.entrySet())
-        {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
-    public static void main(String args[]){
 
-        int arr[] = {10,5,10,15,10,5};
+    public static void main(String args[]) {
+
+        int arr[] = {10, 5, 10, 15, 10, 5};
         int n = arr.length;
         Frequency(arr, n);
         countFreq(arr, n);
