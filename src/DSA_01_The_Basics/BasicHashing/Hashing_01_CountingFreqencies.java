0045c0753c1ebf466255749670694a5054034a7f
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-02-27 21:54:23 +0530

    Create Hashing_01_CountingFreqencies.java

---
+++ src/DSA_01_The_Basics/BasicHashing/Hashing_01_CountingFreqencies.java
@@ -0,0 +1,53 @@
+package DSA_01_The_Basics.BasicHashing;
+
+import java.util.*;
+
+
+public class Hashing_01_CountingFreqencies {
+    public static void countFreq(int arr[], int n) {
+        boolean visited[] = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+
+            if (visited[i] == true)
+                continue;
+
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " " + count);
+        }
+    }
+
+    public static void Frequency(int arr[], int n)
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (map.containsKey(arr[i]))
+            {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }
+            else
+            {
+                map.put(arr[i], 1);
+            }
+        }
+        for (Map.Entry<Integer,Integer> entry : map.entrySet())
+        {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+    public static void main(String args[]){
+
+        int arr[] = {10,5,10,15,10,5};
+        int n = arr.length;
+        Frequency(arr, n);
+        countFreq(arr, n);
+    }
+}
