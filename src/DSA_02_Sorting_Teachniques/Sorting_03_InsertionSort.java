2a4a661bf9072bb955fe5397c4e5e636f29f1f09
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-03-02 00:31:41 +0530

    Update Sorting_03_InsertionSort.java

--- src/DSA_02_Sorting_Teachniques/Sorting_03_InsertionSort.java
+++ src/DSA_02_Sorting_Teachniques/Sorting_03_InsertionSort.java
@@ -1,6 +1,11 @@
 package DSA_02_Sorting_Teachniques;
 
 public class Sorting_03_InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        
+    }
+
     public static void main(String[] args) {
         
     }
