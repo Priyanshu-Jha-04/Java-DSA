2b367ddf3566fba55922c135876fdfef15f928b7
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-03-02 00:31:37 +0530

    Update Sorting_02_BubbleSort.java

--- src/DSA_02_Sorting_Teachniques/Sorting_02_BubbleSort.java
+++ src/DSA_02_Sorting_Teachniques/Sorting_02_BubbleSort.java
@@ -1,6 +1,11 @@
 package DSA_02_Sorting_Teachniques;
 
 public class Sorting_02_BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+
+    }
+
     public static void main(String[] args) {
 
     }
