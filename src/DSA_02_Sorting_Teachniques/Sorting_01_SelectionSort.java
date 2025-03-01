44807f80c4891aad5959c3a6ca28e7ae6ee79206
Priyanshu Jha <priyanshujha2581@gmail.com>
2025-03-01 23:04:09 +0530

    Update Sorting_01_SelectionSort.java

--- src/DSA_02_Sorting_Teachniques/Sorting_01_SelectionSort.java
+++ src/DSA_02_Sorting_Teachniques/Sorting_01_SelectionSort.java
@@ -1,7 +1,35 @@
 package DSA_02_Sorting_Teachniques;
 
 public class Sorting_01_SelectionSort {
-    public static void main(String[] args) {
 
+    static void selectionSort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before selection sort:");
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        selectionSort(arr, n);
     }
 }
